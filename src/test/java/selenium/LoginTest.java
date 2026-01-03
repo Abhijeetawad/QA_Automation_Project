fee923c1b34f9584de287e0e379d4bd38c1bd05d
Abhijeetawad <104734543+Abhijeetawad@users.noreply.github.com>
2026-01-03 14:26:03 +0530

    Update LoginTest.java

--- src/test/java/selenium/LoginTest.java
+++ src/test/java/selenium/LoginTest.java
@@ -14,15 +14,13 @@ public class LoginTest {
 
     @BeforeClass
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhij\\Desktop\\Automation files\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\abhij\\Desktop\\Automation files\\chromedriver-win64\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-infobars", "--disable-extensions");
+        options.addArguments("--start-maximized"); // Open browser maximized
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-extensions");
 
         driver = new ChromeDriver(options);
     }
@@ -35,13 +33,13 @@ public class LoginTest {
         driver.findElement(By.id("password")).sendKeys("Password123");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("submit")));
-        driver.findElement(By.id("submit")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("submit"))).click();
 
         // Validation
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
         String successMessage = driver.findElement(By.tagName("h1")).getText();
-        if(successMessage.contains("Logged In Successfully")) {
+
+        if (successMessage.contains("Logged In Successfully")) {
             System.out.println("Login test executed successfully!");
         } else {
             System.out.println("Login test failed!");
@@ -50,6 +48,8 @@ public class LoginTest {
 
     @AfterClass
     public void teardown() {
-        if(driver != null) driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
