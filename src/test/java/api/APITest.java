127cd007b7e18d48c4ccc95d9c05644870bd23ae
Abhijeetawad <104734543+Abhijeetawad@users.noreply.github.com>
2026-01-03 11:59:13 +0530

    Create APITest.java

---
+++ src/test/java/api/APITest.java
@@ -0,0 +1,18 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class APITest {
+
+    @Test
+    public void getUserTest() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        Response response = RestAssured.get("/users/1");
+        System.out.println("Response: " + response.getBody().asString());
+        assertEquals(response.getStatusCode(), 200);
+    }
+}
