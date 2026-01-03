b604771d3c7c739d64a931fb90cc6fcded53c3c5
Abhijeetawad <104734543+Abhijeetawad@users.noreply.github.com>
2026-01-03 11:57:50 +0530

    Create LoginTest.java

---
+++ src/test/java/selenium/LoginTest.java
@@ -0,0 +1,31 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe"); // Update path
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("https://example.com/login");
+        driver.findElement(By.id("username")).sendKeys("testuser");
+        driver.findElement(By.id("password")).sendKeys("password123");
+        driver.findElement(By.id("loginButton")).click();
+        System.out.println("Login test executed successfully!");
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
+}
